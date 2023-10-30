d271d013a1a50d9606b5ca0bbaa04dc89e08cd2f
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-10-30 19:29:45 +0800

    - add about view - change root route contact to about

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('index');
-    return view('contact');
+    // return view('contact');
+    return view('about');
 });
 
 Route::get('/post', function () {
